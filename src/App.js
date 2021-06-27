8255ed30f49a8d17a80d35eec3eea903c540a1bc
Jared <jared@adadevelopersacademy.org>
2021-06-27 14:42:50 -0700

    Jareds solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import LOCATIONIQ_API_KEY from './secrets.js'
+import LOCATIONIQ_API_KEY from './secrets.js';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState({lat: "no results yet", lon: "no results yet"});
+  const [error, setError] = useState(null);
+
+  const updateSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const callApi = (event) => {
+    event.preventDefault();
+    axios.get(`https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQ_API_KEY}&q=${searchTerm}&format=json`)
+      .then((response) => {
+        setSearchResults(response.data[0]);
+      })
+      .catch((error) => {
+        setError(error.response.data.error);
+      });
+  };
+
+  let errorJSX;
+  if (error != null) {
+    errorJSX = (
+    <section className="error">
+      <h2>Shit! Error!</h2>
+      <p>{error}</p>
+    </section>);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My API key is {LOCATIONIQ_API_KEY} and was loaded from secrets.js!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Get Longitude and Latitude</h1>
+        <form onSubmit={callApi}>
+          <input type="text" value={searchTerm} onChange={updateSearchTerm} />
+          <input type="submit" value="Search Now!" />
+        </form>
+        <h2>Results for {searchTerm}:</h2>
+        <ul>
+          <li>Latitude: {searchResults.lat}</li>
+          <li>Longitude: {searchResults.lon}</li>
+        </ul>
+        {errorJSX}
       </header>
     </div>
   );
