2a59c0dd4704337b739454142f17ed96f400fd4c
Audrey Andoy <audreylandoy@gmail.com>
2021-06-28 09:02:35 -0700

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import LOCATIONIQ_API_KEY from './secrets.js';
-import { useState } from 'react';
+import { useState,  useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
 
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("Seattle");
   const [searchResults, setSearchResults] = useState({lat: "no results yet", lon: "no results yet"});
   const [error, setError] = useState(null);
 
@@ -14,9 +14,19 @@ function App() {
     setSearchTerm(event.target.value);
   };
 
+  useEffect( () => {
+    axios.get(`https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQ_API_KEY.LOCATIONIQ_API_KEY}&q=$Seattle&format=json`)
+    .then((response) => {
+      setSearchResults(response.data[0]);
+    })
+    .catch((error) => {
+      setError(error.response.data.error);
+    });
+  }, []);
+
   const callApi = (event) => {
-    event.preventDefault();
-    axios.get(`https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQ_API_KEY}&q=${searchTerm}&format=json`)
+    event.preventDefault()
+    axios.get(`https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQ_API_KEY.LOCATIONIQ_API_KEY}&q=${searchTerm}&format=json`)
       .then((response) => {
         setSearchResults(response.data[0]);
       })
@@ -24,6 +34,7 @@ function App() {
         setError(error.response.data.error);
       });
   };
+
 
   let errorJSX;
   if (error != null) {
@@ -39,10 +50,12 @@ function App() {
       <header className="App-header">
         <h1>Get Longitude and Latitude</h1>
         <form onSubmit={callApi}>
+          <label htmlFor="Location Name: " />
           <input type="text" value={searchTerm} onChange={updateSearchTerm} />
           <input type="submit" value="Search Now!" />
         </form>
         <h2>Results for {searchTerm}:</h2>
+  
         <ul>
           <li>Latitude: {searchResults.lat}</li>
           <li>Longitude: {searchResults.lon}</li>
